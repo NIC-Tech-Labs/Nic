ec40d766b77fc71c0f1845e31b322cf4e863f71c
Tom <t0malexander@protonmail.com>
2023-08-09 17:15:33 -0300

    feat: :tada: início do projeto

---
+++ mobile/babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src/'],
+          alias: {
+            '@screens': './src/screens',
+            '@theme': './src/theme',
+            '@routes': './src/routes',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
+  }
+}
