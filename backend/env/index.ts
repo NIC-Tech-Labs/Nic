52b522b614444e82813a19452e88b4249365462d
Tom <t0malexander@protonmail.com>
2023-08-13 22:57:51 -0300

    feat: :closed_lock_with_key: criado configs de variáveis

---
+++ backend/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import {z as Zod} from 'zod'
+
+const EnvSchema = Zod.object({
+  NODE_ENV: Zod.enum(['dev', 'prod']).default('dev'),
+  SERVER_PORT: Zod.coerce.number().default(3333),
+  JWT_SECRET: Zod.string()
+})
+
+const _Env = EnvSchema.safeParse(process.env)
+
+if (_Env.success === false) {
+  console.error('🚨 Variáveis de ambiente inválidas!', _Env.error.format())
+
+  throw new Error('🚨 Variáveis de ambiente inválidas!')
+}
+
+export const Env = _Env.data
