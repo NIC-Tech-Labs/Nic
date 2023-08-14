f58f2fab215f437d0157018896414cc32bc8b819
Tom <t0malexander@protonmail.com>
2023-08-13 23:23:22 -0300

    build: :bricks: ajustado arquivos base

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect()
   }
+
+  // async SafeShutdown(app: INestApplication) {
+  //   this.$on('beforeExit', async () => {
+  //     await app.close()
+  //   })
+  // }
 }
