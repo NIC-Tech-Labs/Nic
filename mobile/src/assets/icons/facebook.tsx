e1cef70f78ada618e2550b3e8fc62b8421bcb56a
Tom <t0malexander@protonmail.com>
2023-08-16 14:05:09 -0300

    feat: :bento: adicionado midias e ícones

---
+++ mobile/src/assets/icons/facebook.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Svg, { Path } from 'react-native-svg'
+
+export const FacebookIcon = () => (
+  <Svg width={26} height={25} fill="none">
+    <Path
+      fill="#395185"
+      fillRule="evenodd"
+      d="M25.25 12.779C25.25 6.014 19.765.529 13 .529S.75 6.014.75 12.779c0 6.114 4.479 11.182 10.336 12.102v-8.56H7.975v-3.542h3.11V10.08c0-3.07 1.83-4.766 4.628-4.766 1.34 0 2.743.239 2.743.239v3.015H16.91c-1.52 0-1.996.944-1.996 1.914v2.297h3.397l-.543 3.542h-2.854v8.56c5.857-.919 10.336-5.986 10.336-12.102Z"
+      clipRule="evenodd"
+    />
+  </Svg>
+)
