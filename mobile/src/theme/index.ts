8656815049818885ed9e8919faedd747126bdfb6
Tom <t0malexander@protonmail.com>
2023-08-16 14:06:50 -0300

    feat: :lipstick: criado estilização e cores

---
+++ mobile/src/theme/index.ts
@@ -0,0 +1,46 @@
+export const globalColours = {
+  white: '#FFF',
+  green: '#30973B',
+  red: '#CB2332',
+  blue: '#2554A2',
+  orange: '#EE6F2C'
+}
+
+export const font = {
+  regular: 'DMSans_400Regular',
+  medium: 'DMSans_500Medium',
+  bold: 'DMSans_700Bold'
+}
+
+export const fullWidth = '100%'
+export const defaultLetterSpacing = '-0.35px'
+
+export const light = {
+  backgroundColour: '#FFF',
+
+  text: {
+    default: '#444444',
+    white: '#FFF',
+    gray: '#868686',
+    black: '#00000050'
+  },
+
+  borderColour: {
+    default: '#868686'
+  }
+}
+
+export const dark = {
+  backgroundColour: '#1C1C1C',
+
+  text: {
+    default: '#AFB0B6',
+    white: '#FAFAFA',
+    gray: '#868686',
+    black: '#000',
+  },
+
+  borderColour: {
+    default: '#D1D1D1'
+  }
+}
