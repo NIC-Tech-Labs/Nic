be6466d264bbb2999f8bdf29c012e3a0457ed11f
Tom <t0malexander@protonmail.com>
2023-08-16 14:09:24 -0300

    chore: :truck: criado atalhos de pastas

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -12,6 +12,7 @@ module.exports = function (api) {
             '@theme': './src/theme',
             '@routes': './src/routes',
             '@assets': './src/assets',
+            '@components': './src/components'
           }
         }
       ]
