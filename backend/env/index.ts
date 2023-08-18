5e0befbfac0436127a690e8a38867c04ed96bbd7
Tom <t0malexander@protonmail.com>
2023-08-18 14:44:52 -0300

    build: :wrench: atualizado configs de ambiente

--- backend/env/index.ts
+++ backend/env/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import {z as Zod} from 'zod'
 
 const EnvSchema = Zod.object({
-  NODE_ENV: Zod.enum(['dev', 'prod']).default('dev'),
+  NODE_ENV: Zod.enum(['dev', 'test', 'prod']).default('dev'),
   SERVER_PORT: Zod.coerce.number().default(3333),
   JWT_SECRET: Zod.string()
 })
