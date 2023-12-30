eecb7952d30e2a9850418572b294fec1f24b1714
Tom <tom@saudeagora.com.vc>
2023-12-30 02:33:34 -0300

    style: :art: melhorias do formato do código

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+    dirs: ['./src']
+  }
+}
 
 module.exports = nextConfig
